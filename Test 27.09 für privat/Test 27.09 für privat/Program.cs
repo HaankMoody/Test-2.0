72eca0e2c35370395b63ae0539a31a15e4e5c1d5
TEC\LHO <linus.hohenhaus@tecalliance.net>
2021-09-28 09:59:14 +0200

    Test

---
+++ Test 27.09 für privat/Test 27.09 für privat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_27._09_für_privat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
